90f6d71737de880d943ea8744c30a787eda9ce8c
Rucha Heda <raheda@gmail.com>
2024-10-02 17:20:18 -0700

    implemented ability to add snippets. editing, deleting is not an option yet.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import WaveSurfer from 'wavesurfer.js';
 import TimelinePlugin from 'wavesurfer.js/plugins/timeline';
 import Hover from 'wavesurfer.js/plugins/hover';
 import RegionsPlugin from 'wavesurfer.js/plugins/regions';
-import { AppBar, Box, Button, Checkbox, IconButton, TextField, Toolbar, Typography } from '@mui/material';
-import { CloudUpload, Forward5, Forward10, Forward30, Replay5, Replay10, Replay30 } from '@mui/icons-material';
+import { 
+  AppBar, 
+  Box, 
+  Button, 
+  Checkbox, 
+  IconButton, 
+  Paper, 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  TextField, 
+  Toolbar, 
+  Tooltip,
+  Typography } from '@mui/material';
+import { ArrowCircleUp, CloudUpload, Delete, Edit, Forward5, Forward10, Forward30, Replay5, Replay10, Replay30 } from '@mui/icons-material';
 
 // Define types for state variables
 interface Region {
@@ -19,7 +35,9 @@ function App() {
   const [currentRegionStartTime, setCurrentRegionStartTime] = useState<number>(0);  // State for start time
   const [currentRegionEndTime, setCurrentRegionEndTime] = useState<number>(0);      // State for end time
   const [loopAudio, setLoopAudio] = useState<boolean>(true);
-  const [snippetName, setSnippetName] = useState<string>('');
+  const [regionName, setRegionName] = useState<string>('');
+  const [renderTrigger, setRenderTrigger] = useState(0);
+  const regions = useRef<{[id: number]: any}>({});
   const loopAudioRef = useRef<boolean>(true);
   const wavesurferRef = useRef<WaveSurferInstance | null>(null);
   const regionRef = useRef<any>(null); // Reference for the waveform region
@@ -52,21 +70,20 @@ function App() {
       labelSize: '11px',
     })
 
-    const regions = RegionsPlugin.create();
+    const regionPlugin = RegionsPlugin.create();
 
     const wavesurfer = WaveSurfer.create({
       container: '#waveform',
       waveColor: 'rgb(200, 0, 200)',
       progressColor: 'rgb(100, 0, 100)',
       height: 100,
-      // Set a bar width
       barWidth: 2,
-      // Optionally, specify the spacing between bars
       barGap: 1,
-      // And the bar radius
       barRadius: 2,
-      plugins: [topTimeline, hover, regions]
+      plugins: [topTimeline, hover, regionPlugin]
     });
+
+    console.log("active plugins, ", wavesurfer.getActivePlugins());
 
     wavesurferRef.current = wavesurfer;
 
@@ -76,7 +93,7 @@ function App() {
     wavesurfer.on('ready' as any, () => {
       console.log("waveform is ready!");
 
-      regions.enableDragSelection({
+      regionPlugin.enableDragSelection({
         color: 'rgba(255, 0, 0, 0.3)',
       });
 
@@ -86,40 +103,81 @@ function App() {
       });
   
       // Listen for region creation
-      regions.on('region-created' as any, (region) => {
+      regionPlugin.on('region-created' as any, (region) => {
+        console.log("region-created! region: ", region);
         regionRef.current = region;
-        updateRegion(region.start, region.end);
+
+        if (!region.content) {
+          region.content = document.createElement('div');
+          region.content.textContent = "New Region";
+        }
+        region.contentEditable = true;
+
+        updateRegion(region.start, region.end, region.content?.innerText);
+
+        regions.current = {
+          ...regions.current,
+          [region.id]: region
+        }
+        setRenderTrigger(prev => prev + 1);
       });
 
-      regions.on('region-clicked' as any, (region, e) => {
+      regionPlugin.on('region-clicked' as any, (region, e) => {
         e.stopPropagation(); // prevent triggering a click on the waveform
         regionRef.current = region;
-        updateRegion(region.start, region.end);
+        updateRegion(region.start, region.end, region.content?.innerText);
         region.play();
       });
 
-      regions.on('region-out' as any, (region) => {
-        console.log('region-out, loopAudio:', loopAudioRef);
+      regionPlugin.on('region-out' as any, (region) => {
         if (loopAudioRef.current) {
           region.play();
         }
       })
+
+      regionPlugin.on('region-updated' as any, (region) => {
+        setRenderTrigger(prev => prev + 1);
+      })
     });
   };
 
+  // useEffect(() => {
+  //   console.log("Updated regions: ", regions);
+  // }, [regions]);
+
+  const createRegionManually = () => {
+     const newRegion = (wavesurferRef.current?.getActivePlugins()[2] as RegionsPlugin).addRegion({
+      start: currentRegionStartTime,
+      end: currentRegionEndTime,
+      content: regionName ? regionName : "New Region",
+      contentEditable: true,
+      drag: true,
+      resize: true,
+    });
+
+    regionRef.current = newRegion
+    regions.current = {
+      ...regions.current,
+      [newRegion.id]: newRegion
+    }
+    setRenderTrigger(prev => prev + 1);
+
+  }
+
   // Update region when start and end times are manually or programmatically changed
-  const updateRegion = (newStart: number, newEnd: number) => {
+  const updateRegion = (newStart: number, newEnd: number, name?: string) => {
     if (regionRef.current) {
       setCurrentRegionStartTime(newStart);
       setCurrentRegionEndTime(newEnd);
+      if (name) {
+        setRegionName(name);
+      }
     }
   };
 
   const toggleLooping = () => {
-    // console.log('previous loopAudio value: ', loopAudioRef.current);
     loopAudioRef.current = !loopAudioRef.current;
     setLoopAudio(loopAudioRef.current);
-    // console.log('current loopAudio value: ', loopAudioRef.current);
   }
 
   const handlePlayPause: React.MouseEventHandler<HTMLButtonElement> = () => {
@@ -131,12 +189,7 @@ function App() {
       return;
     }
 
-    const currentTime = wavesurferRef.current.getCurrentTime();
-    const newTime = currentTime + seconds;
-    const duration = wavesurferRef.current.getDuration();
-    if (newTime >= 0 && newTime <= duration) {
-      wavesurferRef.current.seekTo(newTime / duration);
-    }
+    wavesurferRef.current.skip(seconds);
   }
 
   // Handle file upload
@@ -166,7 +219,7 @@ function App() {
       filename: audioFile,
       start: currentRegionStartTime,
       end: currentRegionEndTime,
-      output_name: snippetName,
+      output_name: regionName,
     };
 
     fetch('http://localhost:5001/trim', {
@@ -254,8 +307,10 @@ function App() {
         </Box>
 
         <Box
-          component="form"
-          sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }} 
+          display="flex"
+          flexDirection="row"
+          gap="20px"
+          justifyContent="center"
         >
           <TextField 
             id="outlined-number" 
@@ -279,12 +334,48 @@ function App() {
             id="outlined-basic"
             label="Snippet Name"
             variant="outlined"
-            value={snippetName}
-            onChange={(e) => setSnippetName(e.target.value)}
+            value={regionName}
+            onChange={(e) => setRegionName(e.target.value)}
           />
+
+          <Button variant="outlined" onClick={createRegionManually} color="secondary">Create Snippet</Button>
         </Box>
 
         <Button variant="contained" onClick={handleTrim} color="primary">Trim & Download</Button>
+
+        <Box>
+          <TableContainer component={Paper}>
+            <Table sx={{ width: "80vw"}} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell><b>ID</b></TableCell>
+                  <TableCell><b>Start Time</b></TableCell>
+                  <TableCell><b>End Time</b></TableCell>
+                  <TableCell><b>Snippet Name</b></TableCell>
+                  <TableCell><b>Actions</b></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {/* {console.log(regions)} */}
+                {Object.values(regions.current).map((region, index) => {
+                  console.log("regionsTrigger on render: ", renderTrigger);
+                  return (
+                  <TableRow key={index}>
+                    <TableCell>{region.id}</TableCell>
+                    <TableCell>{region.start}</TableCell>
+                    <TableCell>{region.end}</TableCell>
+                    <TableCell>{typeof region.content == "string" ? region.content : region.content.innerText}</TableCell>
+                    <TableCell>
+                      <Tooltip title="Load"><IconButton size="large"><ArrowCircleUp fontSize="large" color="secondary" /></IconButton></Tooltip>
+                      <Tooltip title="Edit"><IconButton size="large"><Edit fontSize="large" color="primary" /></IconButton></Tooltip>
+                      <Tooltip title="Delete"><IconButton size="large"><Delete fontSize="large" sx={{ color: "#f44336" }} /></IconButton></Tooltip>
+                    </TableCell>
+                  </TableRow>
+                )})}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
     </Box>
   );

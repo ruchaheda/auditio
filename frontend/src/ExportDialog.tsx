f6a4c25203e0b3e7c80b236b626b5ad6043e9864
ruchaheda <raheda@gmail.com>
2024-10-04 00:00:36 -0700

    added ability to share and import snippet text (#11)

---
+++ frontend/src/ExportDialog.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Box,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    IconButton,
+    TextField,
+    Tooltip,
+} from '@mui/material';
+import {
+    Close,
+    ContentCopy,
+} from '@mui/icons-material';
+
+type ExportDialogProps = {
+    regions: any,
+    openExport: boolean,
+    setOpenExport: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const ExportDialog: React.FC<ExportDialogProps> = ({regions, openExport, setOpenExport}) => {
+
+    const [exportText, setExportText] = useState('');
+    const [copied, setCopied] = useState(false);
+
+    // build export text variable
+    useEffect(() => {
+        if (openExport) {
+            buildExport();
+        }
+    }, [openExport]);
+
+    const buildExport = () => {
+        const regionsToLoop: { [id: string]: any } = regions.current;
+        let currentExportText = '';
+
+        Object.entries(regionsToLoop).forEach(([id, region]) => {
+            currentExportText += region.start + ", " + region.end + ", " + region.content?.innerText + "\n";
+        });
+
+        setExportText(currentExportText.replace(/\n$/, ''));
+        console.log("currentExportTest: ", currentExportText);
+    }
+
+    const handleClose = () => { 
+        setOpenExport(false);
+    }
+    
+    const handleCopy = () => {
+        navigator.clipboard.writeText(exportText).then(() => {
+            setCopied(true); // Set copied state to true to show confirmation
+        });
+    }
+
+    return (
+        <Box>
+            <Dialog 
+                open={openExport} 
+                onClose={handleClose}
+                sx={{ '& .MuiDialog-paper': { width: '75%', maxWidth: 'none' } }} // Custom width
+            >
+                <DialogTitle>
+                    Share Snippets!
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                    <Close />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <Box
+                        display="flex"
+                        flexDirection="row"
+                    >
+                    <TextField
+                        label="All Snippets"
+                        multiline
+                        fullWidth
+                        // rows={4}
+                        value={exportText}
+                        slotProps={{
+                        input: {
+                            readOnly: true,
+                        },
+                        }}
+                        variant="filled"
+                    />
+                    <Tooltip title={copied ? 'Copied!' : 'Copy'}>
+                        <IconButton onClick={handleCopy} sx={{ mt: 2 }}>
+                        <ContentCopy />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+                </DialogContent>
+                </Dialog>
+        </Box>
+    );
+}
+
+export default ExportDialog;
---
+++ frontend/src/ImportDialog.tsx
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogTitle,
+    DialogContent,
+    IconButton,
+    TextField,
+} from '@mui/material';
+import {
+    Close,
+} from '@mui/icons-material';
+import WaveSurfer from 'wavesurfer.js';
+import RegionsPlugin from 'wavesurfer.js/plugins/regions';
+
+type ImportDialogProps = {
+    audioFile: string | null,
+    regions: any,
+    wavesurferRef: any,
+    regionRef: any,
+    openImport: boolean,
+    setOpenImport: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const ImportDialog: React.FC<ImportDialogProps> = ({audioFile, regions, wavesurferRef, regionRef, openImport, setOpenImport}) => {
+
+    const [importText, setImportText] = useState('');
+
+    const handleClose = () => { 
+        setOpenImport(false);
+    }
+
+    const handleImport = () => {
+        if (!audioFile) {
+            console.log("no audio file, can't import snippets!");
+            return;
+        }
+
+        const lines = importText.split('\n');
+
+        for (const line of lines) {
+            if (line != "") {
+                const values = line.split(',');
+
+                const newRegionStartTime = Number(values[0]);
+                const newRegionEndTime = Number(values[1]);
+                const newRegionName = values[2];
+
+                const newRegion = (wavesurferRef.current?.getActivePlugins()[2] as RegionsPlugin).addRegion({
+                    start: newRegionStartTime,
+                    end: newRegionEndTime,
+                    content: newRegionName,
+                    contentEditable: true,
+                    drag: true,
+                    resize: true,
+                });
+
+                regions.current[newRegion.id] = newRegion;
+
+                regionRef.current = newRegion;
+            }
+        }
+
+        handleClose();
+    }
+
+    return (
+        <Box>
+            <Dialog
+                open={openImport}
+                onClose={handleClose}
+                sx={{ '& .MuiDialog-paper': { width: '75%', maxWidth: 'none' } }} // Custom width
+            >
+                <DialogTitle>
+                    Import Snippets!
+                    <IconButton
+                        aria-label="close"
+                        onClick={handleClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <Close />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        gap="20px"
+                    >
+                        <TextField
+                            label="Snippets CSV"
+                            multiline
+                            fullWidth
+                            variant="standard"
+                            value={importText}
+                            onChange={(e) => setImportText(e.target.value)}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions sx={{ justifyContent: 'flex-end' }}>
+                    <Button 
+                        variant="contained" 
+                        onClick={handleImport} 
+                        color="secondary"
+                        style= {{
+                            width: '200px',
+                        }}
+                    >
+                        Import Snippets
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Box>
+    );
+}
+
+export default ImportDialog;
